4e56567fb95fc62953b3a6eea2300c68b329db1a
sjsakib <sjsakib.bd@gmail.com>
2018-11-25 21:10:54 +0600

    added custom document;

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,9 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
@@ -12,11 +12,11 @@ export default class MyDocument extends Document {
         <Head>
           <title>Name The Code</title>
         </Head>
-        <body >
+        <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
